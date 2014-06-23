456bcfdc40e15909ad369a0a2af4a87a82c2e359
Justin Butler <butler.jus@gmail.com>
2014-06-22 22:13:15 -0700

    Adding submit and explore templates

--- kyle_app.py
+++ kyle_app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/submit")
+def submit():
+    return render_template('submit.html')
+
+@app.route("/explore")
+def explore():
+    return render_template('explore.html')
+
 if __name__ == "__main__":
     app.run()
